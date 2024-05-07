ca663181d393f5b9db930e53c120eef3e3040817
GoutamKumawat <124769594+GoutamKumawat@users.noreply.github.com>
2024-05-07 16:05:37 +0530

    07-05-2024 search in network

--- app/src/main/java/com/chandra/cargo/ui/network/OurNetworkFragment.kt
+++ app/src/main/java/com/chandra/cargo/ui/network/OurNetworkFragment.kt
@@ -3,6 +3,8 @@ package com.chandra.cargo.ui.network
 import android.annotation.SuppressLint
 import android.app.Dialog
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -18,8 +20,6 @@ import com.chandra.cargo.base.BaseFragment
 import com.chandra.cargo.common.AppState
 import com.chandra.cargo.common.DialogUtils
 import com.chandra.cargo.databinding.FragmentOurNetworkBinding
-import com.chandra.cargo.ui.annoucement.AnnouncementAdapter
-import com.chandra.cargo.ui.helpline.HelpLineViewModel
 import com.chandra.cargo.ui.network.model.CityX
 import com.rdd.rdd.utils.sharedPrefrence.AppPreferences
 import com.rudraansh.rudraapay.utils.sharedPrefrence.Constant
@@ -44,7 +44,9 @@ class OurNetworkFragment : BaseFragment<FragmentOurNetworkBinding>() {
     private lateinit var appPreferences : AppPreferences
     private  lateinit var progress: Dialog
     private  lateinit var userID: String
+    private  lateinit var networkAdapter: NetworkAdapter
     var cityList: List<CityX>? = null
+    private lateinit var adapter: ArrayAdapter<String>
 
     private val viewModel: NetworkViewModel by lazy {
         ViewModelProvider(this)[NetworkViewModel::class.java]
@@ -76,7 +78,7 @@ class OurNetworkFragment : BaseFragment<FragmentOurNetworkBinding>() {
         binding.layoutHeader.tvHeading.text="Our Networks"
         binding.layoutHeader.ivImg.setImageDrawable(mActivity.resources.getDrawable(R.drawable.network))
 
-
+/*
         binding.autocompleteTitle.onItemClickListener = AdapterView.OnItemClickListener { parent, _, position, _ ->
             //   val selectedItem = parent.getItemAtPosition(position) as String
             val selectedState = cityList?.get(position)
@@ -89,10 +91,30 @@ class OurNetworkFragment : BaseFragment<FragmentOurNetworkBinding>() {
             viewModel.NetworkAPI(stateId)
             setUpViewModelObserver()
 
+        }*/
+
+        // Implement TextWatcher to update suggestions
+        binding.textInputCity.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(editable: Editable?) {
+                // Filter suggestions based on user input
+                adapter.filter.filter(editable.toString())
+            }
+        })
+        binding.textInputCity.onItemClickListener = AdapterView.OnItemClickListener { parent, _, position, _ ->
+            val selectedCityName = parent.getItemAtPosition(position).toString()
+            val selectedCity = cityList?.find { it.City == selectedCityName }
+            selectedCity?.let {
+                val cityId = it.CityId
+                val cityName = it.City
+                Log.d("City", "Selected city ID: $cityId, City name: $cityName")
+                viewModel.NetworkAPI(cityId)
+            }
         }
-
-
-viewModel.CityAPI()
+        viewModel.CityAPI()
         viewModel.NetworkAPI("")
 
         setUpViewModelObserver()
@@ -110,12 +132,12 @@ viewModel.CityAPI()
                 is AppState.CitySuccess ->{
                     cityList=response.city.City
                     val stateName = cityList!!.map { it.City }
+                  //  binding.autocompleteTitle.setAdapter(adapter)
+                    adapter = ArrayAdapter(mActivity, android.R.layout.simple_dropdown_item_1line, stateName)
 
-                    // Create ArrayAdapter with shopNames
-                    val adapter = ArrayAdapter(requireContext(), R.layout.state_spinner_item, stateName)
-                    // adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                    // Set adapter to the spinner
-                    binding.autocompleteTitle.setAdapter(adapter)
+                    // Set adapter to AutoCompleteTextView
+                    binding.textInputCity.setAdapter(adapter)
+
                     progress.dismiss()
                 }
                 is AppState.NoInternetConnection ->{
@@ -150,15 +172,19 @@ viewModel.CityAPI()
                     if (response.network.Citylist.size != 0){
                         binding.tvHeadings.visibility = View.VISIBLE
                         binding.layoutResult.visibility = View.VISIBLE
-                    val announcementAdapter = NetworkAdapter(requireActivity())
+                     networkAdapter = NetworkAdapter(requireActivity())
                     binding.rvStat.layoutManager =
                         LinearLayoutManager(mActivity, LinearLayoutManager.VERTICAL, false)
-                    binding.rvStat.adapter = announcementAdapter
-                    announcementAdapter.submitList(response.network.Citylist)
-                }
+                    binding.rvStat.adapter = networkAdapter
+                    networkAdapter.submitList(response.network.Citylist)
+                        networkAdapter.notifyDataSetChanged()
+
+                    }
                     else{
                         binding.tvHeadings.visibility = View.GONE
-                        binding.layoutResult.visibility = View.VISIBLE
+                        binding.layoutResult.visibility = View.GONE
+                        networkAdapter.submitList(emptyList())
+                        networkAdapter.notifyDataSetChanged()
                     }
                 }
                 is AppState.NoInternetConnection ->{
